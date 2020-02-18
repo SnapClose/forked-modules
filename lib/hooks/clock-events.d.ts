ac938568b1c493b4656444e4966e08d95d212083
Kamran Varahramyan <kvarahramyan@snapclose.com>
2020-02-17 18:59:59 -0600

    adds lib

---
+++ lib/hooks/clock-events.d.ts
@@ -0,0 +1,16 @@
+/// <reference types="react" />
+/// <reference types="@emotion/core" />
+import { ElementRef } from '../helpers/types';
+declare type CalcTimeFromAngle = (angle: number, { canAutoChangeUnit, wasTapped, }: {
+    canAutoChangeUnit: boolean;
+    wasTapped: boolean;
+    isInnerClick: boolean;
+}) => void;
+export default function useClockEvents(clock: ElementRef, handleChange: CalcTimeFromAngle): {
+    bind: {
+        onMouseDown: (e: import("react").MouseEvent<HTMLElement, MouseEvent>) => void;
+        onTouchStart: () => void;
+        ref: import("react").MutableRefObject<HTMLDivElement>;
+    };
+};
+export {};
